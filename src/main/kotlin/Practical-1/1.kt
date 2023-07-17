6e0bd3de8f6db048ef18caa5f231731f39d3bd44
student <bhargavdodiya21@gnu.ac.in>
2023-07-17 10:42:42 +0530

    hello

--- src/main/kotlin/Practical-1/1.kt
+++ src/main/kotlin/Practical-1/1.kt
@@ -1,3 +1,24 @@
 fun main(){
-    println("Hello World")
+    var a : Int= 10;
+    var b : String= "Hello";
+    var c : Float= 11.1f;
+    var d : Char= 'A';
+    var e : Boolean= true;
+    var f : Double=96.36;
+    var g :  Long=24524423422435;
+    var h : Short=12;
+    var i : Byte=123;
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+    println(i)
+
+
+
 }
