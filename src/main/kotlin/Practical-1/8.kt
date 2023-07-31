58fddfb46a23e6d9ef87ccca7d3d8c4410af0308
Parth2517 <patelparth88495812@gmail.com>
2023-07-31 20:08:51 +0530

    hello

--- src/main/kotlin/Practical-1/8.kt
+++ src/main/kotlin/Practical-1/8.kt
@@ -1,14 +1,38 @@
-class Student(var Enr_no : Int)
-{
-    var Name : String=""
-    constructor(n:String):this(96)
-    {
-        Name  = n
-    }
-}
-class person()
 fun main(){
-    val a = Student("Parth")
-    println(a.Enr_no)
-    println(a.Name)
+    println("Enr_no : 21012011096 ")
+    var a2= arrayOf(5,4,3,2,1)
+    var a3= intArrayOf(8,9,7)
+    var a4=Array(5){0}
+    println("creste Array using arrayof=${a2.contentToString()}\n")
+    println("creste Array using intarrayof=${a3.contentToString()}\n")
+    println("creste Array using Arry=${a4.contentToString()}\n\n")
+    var a1= arrayListOf<Int>()
+    print("Enter your array size=")
+    var size= readln().toInt()
+    println("enter your $size elements!!")
+    for(i in 0..(size-1)){
+        print("enter a[$i]=")
+        a1.add(readln().toInt())
+    }
+    println("Before sorting array is")
+    a1.forEach{print(it,)}
+    println("\nafter sorting using build-in function\n")
+    a1.sort()
+    a1.forEach{print(it,)}
+    var a5= arrayOf(5,4,3,2,1)
+    println("\nBefore sorting array is\n")
+    a5.forEach { print(it,) }
+    var temp:Int
+    for(i in 0 .. size-1){
+        for(j in 0 .. i){
+            if(a5[i]<a5[j]){
+                temp=a5[i]
+                a5[i]=a5[j]
+                a5[j]=temp
+            }
+        }
+    }
+    println("\nafter sorting without using build-in function\n")
+    a5.forEach { print(it,) }
 }
+
